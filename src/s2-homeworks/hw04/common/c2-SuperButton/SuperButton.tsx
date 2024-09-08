0bc52bf68569bfba0d69eea7319e7aaf796efd37
Artsiom Trukhan <artidnb@gmail.com>
2024-09-08 14:16:51 +0300

    Homework 4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,7 +17,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
+    const finalClassName = s.button + ' ' + (xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default) + ' ' + (disabled && s.disabled)
         // + (disabled
         //         ? ...
         //         : xType === 'red'
